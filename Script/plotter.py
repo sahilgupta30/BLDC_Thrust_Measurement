003811654cec7eab34212f6d7833e8dd1237e4c3
shashankmarch27 <shashankmarch27@gmail.com>
2023-09-19 15:28:21 +0530

    implemented reader code

---
+++ Script/plotter.py
@@ -0,0 +1,21 @@
+import serial
+import csv
+
+ser = serial.Serial(port="COM34", baudrate= 115200, timeout=2)
+
+# Input: File path and the string to add as a row
+file_path = "file.csv"
+while True:
+    input_string = ser.readline()
+
+    # Split the input string by a delimiter (e.g., comma) to create a list of values
+    data = input_string.split(',')
+
+    # Open the CSV file in append mode and write the data as a new row
+    with open(file_path, 'a', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(data)
+
+    print("Row added successfully.")
+
+    
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,41 @@
 #include <esc.h>
 #include "HX711.h"
 
-const int LOADCELL_DOUT_PIN = 19;
-const int LOADCELL_SCK_PIN = 18;
+#define LOADCELL_DOUT_PIN 18
+#define LOADCELL_SCK_PIN 19
 
-esc motor (5,ONESHOT125);
+esc motor (21,ONESHOT125);
 HX711 scale;
 
+int throttle_percentage = 0;
+long reading = 0;
+
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(115200);
   motor.init();
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN,64);
 }
 
 void loop() {
-
-  if (scale.is_ready()) {
+  delay(500);
+  while(throttle_percentage <= 100){
+    for(int i = 0;i < 6;i++){
+      while(!scale.is_ready()){}
+      reading += scale.read();
+    }
+    reading = reading / 5;
+    Serial.print(throttle_percentage);
+    Serial.print(",");
+    Serial.println(reading);
+    reading = 0;
+    throttle_percentage++;
+    motor.write(map(throttle_percentage,0,100,0,1023));
   }
+  while (throttle_percentage>=0)
+  {
+    throttle_percentage--;
+    motor.write(map(throttle_percentage,0,100,0,1023));
+    delay(10);
+  }
+  while(true){}
 }
